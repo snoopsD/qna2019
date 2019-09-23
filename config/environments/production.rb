a21e25ab6d9dfd9a8268b19ef77e04646a314ce2
blasta <snoops@list.ru>
2019-09-23 12:43:03 +0400

    change action mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: 465,
+    authentication: 'plain',
+    domain: '51.15.121.165', 
+    user_name: Rails.application.credentials[Rails.env.to_sym][:smtp][:login],
+    password: Rails.application.credentials[Rails.env.to_sym][:smtp][:password],
+    enable_starttls_auto: true
+  }
+  config.action_mailer.default_url_options = { host: '51.15.121.165' }
 end
