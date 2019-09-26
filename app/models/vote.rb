e7717bbb0453b8e66db6716089cd6bd8552b4023
blasta <snoops@list.ru>
2019-09-26 14:58:57 +0400

    add cache

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,5 +1,5 @@
 class Vote < ApplicationRecord
-  belongs_to :votable, polymorphic: true
+  belongs_to :votable, polymorphic: true, touch: true
   belongs_to :user
 
   validates :user_id, uniqueness: { scope: [:votable_id, :votable_type] }
